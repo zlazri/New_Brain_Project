cf78270d63d4d91862c494538df1e4f998b6695a
zlazri <zlazri@terpmail.umd.edu>
2017-11-22 13:43:18 -0500

    Add files via upload

---
+++ gaussian_filter2D.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def gauss_filt2D(n, sig):
+
+    ''' Creates a 2D gaussian kernel.
+      
+        n = size of gaussian kernel-- n x n
+        sig = sigma
+    '''
+
+    nx = ny = np.linspace(-np.floor(n/2), np.floor(n/2), n)
+
+    X, Y = np.meshgrid(nx, ny)
+
+    kernel = np.exp(-(np.square(X) + np.square(Y)) / (2*sig*sig))
+
+    kernel = kernel/np.sum(kernel)
+
+    return kernel
