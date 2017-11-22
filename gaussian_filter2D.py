b75d71e2ae1761ede0463b5e93fb2d2d5520c9e5
zlazri <zlazri@terpmail.umd.edu>
2017-11-22 16:47:11 -0500

    Add files via upload

--- gaussian_filter2D.py
+++ gaussian_filter2D.py
@@ -1,6 +1,16 @@
 import numpy as np
 
-def gauss_filt2D(n, sig):
+def gauss_kern1D(n, sig):
+
+    ''' Creates a 1D gaussian kernel.
+      
+        n = size of gaussian kernel
+        sig = sigma
+    '''
+
+    
+
+def gauss_kern2D(n, sig):
 
     ''' Creates a 2D gaussian kernel.
       
@@ -17,3 +27,36 @@ def gauss_filt2D(n, sig):
     kernel = kernel/np.sum(kernel)
 
     return kernel
+
+def gauss_filt2D(img, Gfilt):
+
+    ''' This function performs the 2D convolution of an image with a Gaussian kernel
+    
+    img = image that must be filtered
+    Gfilt = Gaussian kernel
+
+    Note: m1 and m2, below, should be the same since images are square-- at least for our purposes.
+
+    '''
+
+    m1, m2 = img.shape
+    n1, n2 = Gfilt.shape    
+    r = int(n1/2)
+#    print(r)
+    outimg = np.zeros((m1,m2), dtype=float)
+    I = np.linspace(-r,r,n1)
+    I = I.astype(int)
+#    print(I)
+    i0 = r+1
+    center = (i0, i0)
+    for i in range(r, m1-r-1):
+        for j in range(r, m2-r-1):
+            outimg[i,j] = np.sum(np.multiply(Gfilt,img[i-I, j-I]))
+#            print(outimg[i,j])
+#            assert(j<5)
+
+#    for i in range(r) + range(m1-r-1, m1):
+#        for j in range(r) + range(m2-r-1, m2):
+#            outimg[i,j] = img[i,j]
+
+    return outimg
