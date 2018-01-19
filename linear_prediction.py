0e80f41e7ccdc449363db5b3b689760d524f01ac
zlazri <zlazri@terpmail.umd.edu>
2018-01-19 16:04:35 -0500

    Add files via upload

--- linear_prediction.py
+++ linear_prediction.py
@@ -2,47 +2,63 @@ import numpy as np
 from numpy.linalg import inv
 from numpy.linalg import solve
 
-M = np.load('M_positions.npy')
+def LinPredictor(path, nseq, start, N, K, alp, skip):
+    ''' Path: Path to the sequence data
+        nseq: The index of the blob sequence
+        start: The first frame of the sequence at which we will compute a blob location prediction
+        N: Specifies the number of frames over which we wish to preform blob location estimations
+        K: The number of previous frames taken into account to perform blob location estimate
+        alp: Specifies how many frames into the future we wish to predict
+        skip: Number of starting frames to skip over
+    '''
+    
+    M = np.load(path)
+    n = 1
+    correct = 0
+    incorrect = 0
+    total = N-skip
+    
+    for i in range(start + K - 1, start + N):
+        # Compute sample mean vector
+        mu = np.zeros((K + alp, 1))
+        for j in range(start + K - 1, i + 1):
+            u = np.array([M[nseq, j-K+1:j+1+alp]]).T
+            mu += u
+        mu /= n
 
-N = 200
-K = 2
+        # Compute sample covariance/autocorrelation matrix
+        C = np.zeros((K + alp, K + alp))
+        for j in range(start + K - 1, i + 1):
+            u = np.array([M[nseq, j-K+1:j+1+alp]]).T
+            C += (u - mu)*(u - mu).T
+            # C += u*u.T
+        C /= n
+        # print(C)
+        C += mu*mu.T
 
-n = 1
-for i in range(K - 1, N):
-    # Compute sample mean vector
-    mu = np.zeros((K, 1))
-    for j in range(K - 1, i + 1):
-        u = np.array([M[1, j-K+1:j+1]]).T
-        mu += u
-    mu /= n
+        # print(u)
+        # print(C)
 
-    # Compute sample covariance/autocorrelation matrix
-    C = np.zeros((K, K))
-    for j in range(K - 1, i + 1):
-        u = np.array([M[1, j-K+1:j+1]]).T
-        C += (u - mu)*(u - mu).T
-        # C += u*u.T
-    C /= n
-    # print(C)
-    C += mu*mu.T
+        if i >= skip:
+            # Set up system
+            R = C[0:K-1,0:K-1]
+            r_x = C[1+alp:K+alp, 0]
+            w = solve(R, r_x)
+            # print(w)
 
-    # print(u)
-    # print(C)
+            # Filter
+            predicted = np.around(np.dot(np.flipud(w), M[nseq, i-K+2:i+1]))
+            actual = M[nseq, i+alp]
+            print((predicted, actual))
 
-    if i > 100:
-        # Set up system
-        R = C[0:K-1,0:K-1]
-        r_x = C[1:K, 0]
-        w = solve(R, r_x)
-        # print(w)
-
-        # Filter
-        print((np.dot(np.flipud(w), M[1, i-K+2:i+1]), M[1, i]))
-
-        xi = C[0, 0] - np.dot(w, C[1:K, 0])
-        # print(xi)
-    n += 1
+            # Compute number of correct predictions
+            if predicted == actual:
+                correct += 1
+                        
+            #xi = C[0, 0] - np.dot(w, r_x)    #C[1:K, 0])
+            #print(xi)
+        n += 1
  
  
-# TODO: Now do the multi-step method (or add it as an option to the code above).
-# See page 346 of textbook.
+        # Percentage of correct predictions
+    print("Percentage of correct predictions: "+ str(correct/total))
