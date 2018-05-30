55062c8bbb777a5ec16ae65bca1956e194b01246
zlazri <zlazri@terpmail.umd.edu>
2018-05-30 10:49:49 -0400

    Add files via upload

--- redo_LP.py
+++ redo_LP.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.linalg import toeplitz
 from scipy.linalg import inv
+from matplotlib import pyplot as plt
 
 def reverse(x):
     x = x[::-1]
@@ -77,11 +78,20 @@ if __name__ == "__main__":
 
     correct = 0
     total = 0
+
+    pred_arr = []
+    actual_arr = []
     
     mseqs = np.load("M_positions_new.npy")
     nseqs = np.load("N_positions_new.npy")
     M = mseqs[7, 0:400]
     N = nseqs[7, 0:400]
+
+#    Test Data
+#    M = np.linspace(1, 100, 400)
+#    N = np.linspace(0, 2*np.pi, 400)
+#    N = np.sin(N)
+
     for i in range(100):
         Msamp = M[i:200+i]
         Nsamp = N[i:200+i]
@@ -92,73 +102,23 @@ if __name__ == "__main__":
         # Actual
         actual = np.array([[M[200+i]],[N[200+i]]])
 
-        # Compare results
+        # Percent Correct
         if np.around(pred)[0] == actual[0] and np.around(pred)[1] == actual[1]:
             correct += 1
         total += 1
     print(str(correct/total))
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-#if __name__ == "__main__":
+#        # Create arrays
+#        pred_arr.append((pred[0][0], pred[1][0]))
+#        actual_arr.append((M[200+i], N[200+i]))
 #
-#    mseqs = np.load("M_positions_new.npy")
-#    nseqs = np.load("N_positions_new.npy")
-#    M = mseqs[7, 0:400]
-#    N = nseqs[7, 0:400]
-#    Msamp = M[0:200]
-#    Nsamp = N[0:200]
-#
-#    # Reverse seqs
-#    Msamp = reverse(Msamp)
-#    Nsamp = reverse(Nsamp)
-#
-#    # Convert seqs to mean zero
-#    Mmu0 = mean_zero(Msamp)
-#    Nmu0 = mean_zero(Nsamp)
-#
-#    # Create multichannel vector
-#    x = np.array([Mmu0, Nmu0])
-#    x = restruct(x)
-#
-#    # Create System (find R matrix and r vector)
-#    R = Covar(x, 5)   # <----- play around with second paramameter
-#    L = R.shape[0]
-#    r = R[0:2, 2:].T
-#    RL = R[0:L-2, 0:L-2]  # <---- since we are using 2 channels, subtract 2 instead of 1!!!!!
-#
-#    # Solve System
-#    Rinv = inv(RL)
-#    A = np.matmul(Rinv, r)
-#    
-#    # Predict
-#    xL = np.array([x[0:L-2, 0]]).T
-#    muM = np.mean(Msamp)
-#    muN = np.mean(Nsamp)
-#    mu = np.array([[muM,muN]]).T
-#    pred = np.matmul(A.T, xL)
-#    pred = pred + mu
-#    actual = np.array([[M[200], N[200]]]).T
-#    print(pred)
-#    print(actual)
+#    pred_arr = np.asarray(pred_arr)
+#    actual_arr = np.asarray(actual_arr)
+
+#    print(pred_arr)
+#    print(actual_arr)
+
+#    # Plots
+#    plt.plot(pred_arr[:,0], pred_arr[:,1])
+#    plt.plot(actual_arr[:,0], actual_arr[:,1])
+#    plt.show()
